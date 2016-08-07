48919bf59ce226fc88772caa91a77cbec1d0a500
hexafluoride <xafluoride@gmail.com>
2016-08-07 15:11:11 +0300

    Implement better options and close #1

--- Ethanol/Program.cs
+++ Ethanol/Program.cs
@@ -7,41 +7,74 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+using NDesk.Options;
+using System.Runtime.InteropServices;
+
 namespace Ethanol
 {
     class Program
     {
         static void Main(string[] args)
         {
-            WaveGenerator gen = new WaveGenerator() { Amplitude = 0.25 };
-            WaveFile file = new WaveFile("./out.wav");
+            int sample_rate = 44100;
+            int bit_depth = 64;
+            WaveFormat format = WaveFormat.FloatingPoint;
 
-            if(args.Length < 4)
+            int data_rate = 1200;
+            int low_freq = 1200;
+            int high_freq = 2200;
+
+            double amplitude = 0.25;
+
+            string input_filename = "";
+
+            OptionSet set = null;
+            set = new OptionSet()
             {
-                Console.WriteLine("Usage: ethanol <input file name> <baud> <low bit freq> <high bit freq>");
+                {"h|?|help", "Show help for arguments.", a => ShowHelp(set) },
+                {"sr=|sample-rate=", "Output sample rate.", s => sample_rate = int.Parse(s) },
+                {"b=|bit-depth=", "Output bit depth(8, 16, 32 or 64).", b => bit_depth = int.Parse(b) },
+                {"f=|wave-format=", "Output wave format(pcm or floatingpoint).", f => format = (WaveFormat)Enum.Parse(typeof(WaveFormat), f, true) },
+                {"r=|baud=", "Data rate to modulate the carrier wave at.", r => data_rate = int.Parse(r) },
+                {"lf=|low-freq=", "Frequency to encode for a low bit(0).", l => low_freq = int.Parse(l) },
+                {"hf=|high-freq=", "Frequency to encode for a high bit(1).", h => high_freq = int.Parse(h) },
+                {"a=|amplitude=", "Output waveform's amplitude(0.0-1.0).", a => amplitude = int.Parse(a) },
+                {"i=|input=", "Input file for modulation.", i => input_filename = i }
+            };
+
+            string output_filename = set.Parse(args).LastOrDefault();
+
+            if(string.IsNullOrWhiteSpace(output_filename) || string.IsNullOrWhiteSpace(input_filename))
+            {
+                if (string.IsNullOrWhiteSpace(output_filename))
+                    Console.WriteLine("Invalid filename {0}.", output_filename);
+                if (string.IsNullOrWhiteSpace(input_filename))
+                    Console.WriteLine("Invalid filename {0}.", input_filename);
+
                 return;
             }
 
-            DataReader reader = new DataReader(File.ReadAllBytes(args[0]));
+            WaveGenerator gen = new WaveGenerator() { Amplitude = amplitude };
+            WaveFile file = new WaveFile(output_filename);
+            file.FrameBuffer.SetLength(0);
 
-            int data_rate = int.Parse(args[1]);
-            int low_freq = int.Parse(args[2]);
-            int high_freq = int.Parse(args[3]);
+            file.Format = format;
+            file.SampleRate = (uint)sample_rate;
+            file.BitDepth = (uint)bit_depth;
 
-            int min_samples = 44100 / low_freq;
-            int max_rate = 44100 / min_samples;
+            DataReader reader = new DataReader(File.ReadAllBytes(input_filename));
 
-            if (data_rate > max_rate)
+            if (data_rate > low_freq)
             {
-                Console.WriteLine("{0} Hz can encode {1} baud at most. Limiting data rate to {1} baud.", low_freq, max_rate);
-                data_rate = max_rate;
+                Console.WriteLine("{0} Hz can encode {0} baud at most. Limiting data rate to {0} baud.", low_freq);
+                data_rate = low_freq;
                 Console.WriteLine("Hint: to encode data at X baud, you need at least X Hz on the low bit frequency.");
             }
 
             reader.DataRate = data_rate;
 
             Console.WriteLine("Data density: {0} samples per bit({1} bits/s)", reader.SamplesPerBit, reader.SampleRate / reader.SamplesPerBit);
-            Console.WriteLine("Going to write {0} samples({1:0.00} seconds) of audio.", reader.SamplesPerBit * 8 * reader.Data.Length, (reader.SamplesPerBit * 8 * reader.Data.Length) / 44100d);
+            Console.WriteLine("Going to write {0} samples({1:0.00} seconds) of audio.", reader.SamplesPerBit * 8 * reader.Data.Length, (reader.SamplesPerBit * 8 * reader.Data.Length) / (double)sample_rate);
 
             int x = Console.CursorLeft;
             int y = Console.CursorTop;
@@ -50,15 +83,21 @@ namespace Ethanol
             {
                 gen.Frequency = reader.Step() == 1 ? high_freq : low_freq;
                 file.WriteFrame(gen.Step());
-                if (i % 44100 == 0)
+                if (i % sample_rate == 0)
                 {
-                    Console.WriteLine("{0} seconds...", i / 44100);
+                    Console.WriteLine("{0} seconds...", i / sample_rate);
                     Console.SetCursorPosition(x, y);
                 }
             }
 
             file.Save();
             Console.WriteLine("\ndone!");
+        }
+
+        static void ShowHelp(OptionSet set)
+        {
+            set.WriteOptionDescriptions(Console.Out);
+            Environment.Exit(0);
         }
     }
 }
